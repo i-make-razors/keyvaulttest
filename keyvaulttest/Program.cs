e74591ea1491685064e4121f77045a0d94b1b856
Colin Dickerson <dickerson.cg@pg.com>
2022-12-02 14:24:04 -0500

    added source

---
+++ keyvaulttest/Program.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Identity;
+
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder();
+builder.AddEnvironmentVariables();
+
+var envConfig = builder.Build();
+
+var keyVaultUri = envConfig["KEYVAULT_URI"];
+
+builder.AddAzureKeyVault(new Uri(keyVaultUri), new DefaultAzureCredential());
+
+Console.WriteLine("Building Configuration");
+
+var config = builder.Build();
+
+var configValue = config["AzureKeyVaultTest"];
+
+Console.WriteLine(configValue);
